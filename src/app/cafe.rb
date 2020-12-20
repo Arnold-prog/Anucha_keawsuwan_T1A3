a8ddd92a51b00e84ea95a90eb26ef95393beeee3
Arnold-prog <arnold96k@gmail.com>
2020-12-20 19:19:31 +1000

    add get order method

--- src/app/cafe.rb
+++ src/app/cafe.rb
@@ -32,4 +32,36 @@ def main()
 	puts "-"*40
 end
 
+def print_order(order_hash) # This method shows what the customer has ordered 
+	puts " "
+	puts "Your current order:"
+	if $order_hash.empty?
+		puts "There is nothing currently in your order."
+	else
+		$order_hash.map do |k,v| 
+			puts "x #{v}	#{$item_names[k]} 	= $#{v * $cost_price[k]}"
+		end
+	end
+	puts " "
+	puts "	Total Bill: $#{calculate_total_bill($order_hash)}"
+	puts "-"*40
+    return
+end
 
+def get_order() # This methodd is for customers to add quantity and has an exit app option
+    system "clear"
+      print_intro()
+      print_order($order_hash)
+      quit = false
+      until quit
+          
+      order = ask_order()
+      return quit = true if order == "q"
+      amount = ask_amount()
+      puts amount
+      $order_hash.key?(order) ? $order_hash[order] += amount : $order_hash[order] = amount
+      system "clear"
+      print_intro()
+      print_order($order_hash)
+      quit = follow_up()
+      end
