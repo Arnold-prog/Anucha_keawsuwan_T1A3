8a356b9e11c6f219f265735f37f9e1d7f9d55d9c
Arnold-prog <arnold96k@gmail.com>
2020-12-20 20:08:48 +1000

    added ask order

--- src/app/cafe.rb
+++ src/app/cafe.rb
@@ -1,10 +1,9 @@
 $cost_price = {"l" => 6, "s" => 7, "t" => 8}
-$item_names = {"l"=> "Loving dog", "s"=> "Scooby Snacks", "t"=> "Tofu Special", "q"=> "Quit"}
+$item_names = {"l"=> "Lick my paws", "s"=> "Scooby Snacks", "t"=> "Tofu Special", "q"=> "Quit"}
 $order_hash = {}
 $password = "password"
-#Some main hashes
 
-def main() 
+ def main() #This method is where the menu will show up for their options 
 	quit = false
 	until quit
 		print_intro()
@@ -19,8 +18,10 @@ def main()
 		end
 	end
   system "clear"
+end
 
-  def print_options() #This method is where the menu will show up for their options 
+ 
+def print_options() #This method is where the menu will show up for their options
 	puts "-"*40
 	puts " "
     puts "Special Menu:"
@@ -31,6 +32,7 @@ def main()
 	end
 	puts "-"*40
 end
+
 
 def print_order(order_hash) # This method shows what the customer has ordered 
 	puts " "
@@ -45,37 +47,39 @@ def print_order(order_hash) # This method shows what the customer has ordered
 	puts " "
 	puts "	Total Bill: $#{calculate_total_bill($order_hash)}"
 	puts "-"*40
-    return
+	return
 end
 
-def get_order() # This methodd is for customers to add quantity and has an exit app option
-    system "clear"
-      print_intro()
-      print_order($order_hash)
-      quit = false
-      until quit
-          
-      order = ask_order()
-      return quit = true if order == "q"
-      amount = ask_amount()
-      puts amount
-      $order_hash.key?(order) ? $order_hash[order] += amount : $order_hash[order] = amount
-      system "clear"
-      print_intro()
-      print_order($order_hash)
-      quit = follow_up()
-      end
-      #for the exit farewell and option to exit
-      system "clear"
-      print_order($order_hash)
-      puts " "
-      puts "Thank you for visitings Tofu's Cafe"
-      puts "Type 'quit' to exit."
-      print "Input: "; exit = gets.strip.downcase[0]
-      exit == "q" ? (return true) : (return false)          
-  end
 
-  def calculate_total_bill(order_hash) #Method show order and show total as integer
+def get_order() # This methodd is for customers to add quantity and has an exit app option
+  system "clear"
+	print_intro()
+	print_order($order_hash)
+	quit = false
+	until quit
+
+	order = ask_order()
+	return quit = true if order == "q"
+	amount = ask_amount()
+	puts amount
+	$order_hash.key?(order) ? $order_hash[order] += amount : $order_hash[order] = amount
+	system "clear"
+	print_intro()
+	print_order($order_hash)
+	quit = follow_up()
+	end
+	#for the exit farewell and option to exit
+	system "clear"
+	print_order($order_hash)
+	puts " "
+	puts "Thank you for visitings Tofu's Cafe"
+	puts "Type 'quit' to exit."
+	print "Input: "; exit = gets.strip.downcase[0]
+	exit == "q" ? (return true) : (return false)
+			
+end
+
+def calculate_total_bill(order_hash) #Method show order and show total as integer
 	total = 0
 	order_hash.map do
 		|k,v|
@@ -84,19 +88,26 @@ def get_order() # This methodd is for customers to add quantity and has an exit 
 	return total
 end
 
-def ask_amount()#Method to ask for quantity of order.
+		
+		
+		
+def ask_amount() #Method to ask for quantity of order.
+	
 	puts " "
 	puts "How many would you like to order?"
 	print "Input: "; amount = gets.strip.to_i
 	puts " "
+
 	if amount == 0 || Integer(amount) == nil
-		puts "Sorry, that is an invalid input."
+		puts "Sorry, that is an invalid entry."
 		sleep 1
 		ask_amount()
-	end	
+	end
+		
 	return amount
 end
 
+	
 def ask_order() # ask for order, gives exit option.
 	puts " "
 	puts "What would you like to order? Or type 'quit' to exit."
@@ -107,7 +118,7 @@ def ask_order() # ask for order, gives exit option.
 
 	unless order == "l" || order == "t" || order == "s" || $item_names.key?(order) #if error gives them a way to go again
 		puts "Sorry that is not a valid order. Please try again."
-		sleep 2
+		sleep 1
 		system "clear"
 		print_intro()
 		print_order($order_hash)
@@ -116,10 +127,37 @@ def ask_order() # ask for order, gives exit option.
 	return order
 end
 
+
+
+def user_option()
+	puts " "
+	puts "What would you like to do?"
+	puts " "
+    puts "Order? or Quit? Please type answer below"
+    puts " "
+    
+	
+	print "Input: "; resp = gets.strip.downcase[0]
+	puts resp
+	
+	unless resp == "o" || resp == "q" || resp == "s"
+		puts "Invalid entry. Starting again."
+		sleep 2
+		main()
+	end
+	
+	return resp
+end
+
+
+
+
 def follow_up() #Method follow up if the input is not correct shows again.
     puts "To continue ordering type 'continue'"
     puts "To finish ordering tpye 'finished'"
+
 	print "Input: "; resp = gets.strip.downcase[0]
+	
 	if resp == 'c'
 		print_intro()
 		print_order($order_hash)
@@ -132,22 +170,30 @@ def follow_up() #Method follow up if the input is not correct shows again.
 	end
 end
 
-def print_intro()# Welcome method.
+
+
+
+def print_intro() # Welcome method.
     system "clear"
 	puts "Hi, welcome to Tofu's Cafe ^_^."
 	puts " "
 	print_options()
 end
 
+
+
+
 def confirm_quit() #Method to confirm quit
 	system "clear"
 	puts "Are you sure you want to exit?"
 	puts "y/n"
 	print "Input: "; ans = gets.strip.downcase[0]
-	ans == 'y' ? (return true) : (return false)	
+	ans == 'y' ? (return true) : (return false)
+	
 end
 
-def staff_mode() #Metho for staff mode allows add, remove and change menu.
+
+def staff_mode() #Method for staff mode allows add, remove and change menu.
 	system "clear"
 	puts "You're in staff mode now!"
 	puts "To continue, please enter password."
@@ -177,7 +223,19 @@ def staff_mode() #Metho for staff mode allows add, remove and change menu.
 	return false
 end
 
-def staff_options()#Method for staff options.
+
+
+def print_staff() #shows them in staff mode
+	system "clear"
+	puts "Staff Mode"
+	puts " "
+	print_options()
+end
+
+
+
+
+def staff_options() #Method for staff options.
 	puts " "
     puts "Do you want to.. Type the first word to begin"
 	puts "Add an item?"
@@ -185,11 +243,12 @@ def staff_options()#Method for staff options.
 	puts "Change prices?"
 	puts "Exit?"
 	puts " "
+
 	print "Input: "; option = gets.strip.downcase[0]
 	return option
 end
 
-def staff_add_item()#Metho for staff to add items.
+def staff_add_item() #Metho for staff to add items.
 	print_staff()
 	puts " "
 	puts "What is the new item you would like to add?"
@@ -202,7 +261,7 @@ def staff_add_item()#Metho for staff to add items.
 	return false
 end
 
-def staff_remove_item()#Method for staff to remove items.
+def staff_remove_item() #Method for staff to remove items.
 	print_staff()
 	puts " "
 	puts "What item would you like to remove?"
@@ -216,3 +275,26 @@ def staff_remove_item()#Method for staff to remove items.
 	$cost_price.delete(rem[0])
 	return false
 end
+
+def staff_change_price() #methos to change pirce 
+	print_staff()
+	puts " "
+	puts "What item would you like to change the price of?"
+	$item_names.each_value do |v|
+		next if v == "Quit"
+		puts "[#{v[0]}]#{v[1..v.length]}"
+	end
+	puts " "
+	print "Input: "; change = gets.strip.downcase
+	puts " "
+	puts "What is the new price of the item?"
+	print "Input: "; change_price = gets.strip.to_i
+	$cost_price[change[0]] = change_price
+	return false
+end
+
+
+
+
+
+main()
